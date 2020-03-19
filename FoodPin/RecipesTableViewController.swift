3e66f9c40e0080fd377f2a08fac4bcce57a5f6bd
AlexBulavin <bulavin1970@icloud.com>
2020-03-19 08:33:45 +0300

    Почти законченный проект

--- FoodPin/RecipesTableViewController.swift
+++ FoodPin/RecipesTableViewController.swift
@@ -12,13 +12,21 @@ class RecipesTableViewController: UITableViewController {
 
     var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery" , "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
 
+    var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkeerestaurant", "posatelier", "bourkestreetbakery", "haighschocolate", "palominoespresso", "upstate", "traif", "grahamavenuemeats", "wafflewolf", "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "caskpubkitchen"]
+    
+    var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Ko ng", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney", "New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
+    
+    var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", " British", "Thai"]
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cellIdentifier = "datacell"
-    let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+    let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! RecipeTableViewCell
     // Configure the cell...
-    cell.textLabel?.text = restaurantNames[indexPath.row]
-    cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+    cell.nameLabel?.text = restaurantNames[indexPath.row]
+    cell.thumbnailImageView?.image = UIImage(named: restaurantImages[indexPath.row])
+    cell.locationLabel?.text = restaurantLocations[indexPath.row]
+    cell.typeLabel?.text = restaurantTypes[indexPath.row]
+        
     return cell }
 
     override func numberOfSections(in tableView: UITableView) -> Int { return 1
