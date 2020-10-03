e98297d6d4018aa594d89656cf2614978fd47a28
AlexBulavin <bulavin1970@icloud.com>
2020-10-03 19:57:33 +0300

    Добавлен an action sheet на экране детализации рецепта.

--- FoodPin/Controller/RecipeDetailViewController.swift
+++ FoodPin/Controller/RecipeDetailViewController.swift
@@ -92,25 +92,25 @@ class RecipeDetailViewController: UIViewController, UITableViewDataSource, UITab
     
     //Функция нажатия на экран (в любом месте). Подымает ActionSheet
 //    @IBAction func clickOnScreen(_ sender: UIButton) {
-//
-//        let optionMenu = UIAlertController(title: nil, message: "Что Вы хотите сделать?", preferredStyle: .actionSheet) //.alert) это опциональное оформление сообщения. Алерт или actionSheet
-//        optionMenu.view.tintColor = UIColor(red: 90.0/255.0, green: 45.0/255.0 , blue: 128.0/255.0, alpha: 1.0) //Изменить цвет текста сообщения на фирменный 90;45;128
-//        optionMenu.view.backgroundColor = UIColor(red: 90.0/255.0, green: 45.0/255.0 , blue: 128.0/255.0, alpha: 0.85)//Изменить цвет фона сообщения
-//
-//
-//        // Add actions to the menu
-//         let callActionHandler = { (action:UIAlertAction!) -> Void in
-//             let alertMessage = UIAlertController(title: "Сервис временно недоступен", message: "Извините, сервис пока не доступен, пожалуйста попробуйте позже.",
-//                                                  preferredStyle: .alert)
-//             alertMessage.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-//             alertMessage.view.tintColor = UIColor(red: 90.0/255.0, green: 45.0/255.0 , blue: 128.0/255.0, alpha: 0.85)//Изменить цвет текста ОК
-//             self.present(alertMessage, animated: true, completion: nil)
-//         }
-//
-//         // Add Call action
-//         let callAction = UIAlertAction(title: "Позвонить " + "123-000- (indexPath.row)" , style: .default, handler: callActionHandler) //вместо \(indexPath.row) нужно передать адекватный ID рецепта
-//         optionMenu.addAction(callAction)
-//
+ func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let optionMenu = UIAlertController(title: nil, message: "Что Вы хотите сделать?", preferredStyle: .actionSheet) //.alert) это опциональное оформление сообщения. Алерт или actionSheet
+        optionMenu.view.tintColor = UIColor(red: 90.0/255.0, green: 45.0/255.0 , blue: 128.0/255.0, alpha: 1.0) //Изменить цвет текста сообщения на фирменный 90;45;128
+        optionMenu.view.backgroundColor = UIColor(red: 90.0/255.0, green: 45.0/255.0 , blue: 128.0/255.0, alpha: 0.85)//Изменить цвет фона сообщения
+
+
+        // Add actions to the menu
+         let callActionHandler = { (action:UIAlertAction!) -> Void in
+             let alertMessage = UIAlertController(title: "Сервис временно недоступен", message: "Извините, сервис пока не доступен, пожалуйста попробуйте позже.",
+                                                  preferredStyle: .alert)
+             alertMessage.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+             alertMessage.view.tintColor = UIColor(red: 90.0/255.0, green: 45.0/255.0 , blue: 128.0/255.0, alpha: 0.85)//Изменить цвет текста ОК
+             self.present(alertMessage, animated: true, completion: nil)
+         }
+
+         // Add Call action
+         let callAction = UIAlertAction(title: "Позвонить " + "123-000- (indexPath.row)" , style: .default, handler: callActionHandler) //вместо \(indexPath.row) нужно передать адекватный ID рецепта
+         optionMenu.addAction(callAction)
+
 //        // Добавить/убрать в избранное
 //        let checkInTitle = self.recipe.recipeIsLiked ? "Удалить из избранноого" : "Добавить в избранное"
 //        let checkInAction = UIAlertAction(title: checkInTitle, style: .default, handler: {
@@ -124,33 +124,33 @@ class RecipeDetailViewController: UIViewController, UITableViewDataSource, UITab
 //        })
 //
 //        optionMenu.addAction(checkInAction)
-//
-//        //Add Share recipe action. Делимся рецептом с друзьями.
-//        let shareRecipeAction = UIAlertAction(title: "Поделиться", style: .default, handler: {
-//            (action:UIAlertAction!) -> Void in
-//
-//            let defaultText = "Рекомендую попробовать:" + "\n" + self.recipe.recipeNames + "\n" + "Кухня: " + self.recipe.recipeType + "\n" + "Автор рецепта: " + self.recipe.recipeAuthorLocations + "\n" + "Способ приготовления: " + "\n" + self.recipe.recipeDescription + "\n\n" + "Состав блюда:" + "\n" + self.recipe.recipeIngredients + "\n\n" + "Рецепт доступен в мобильном приложении:" + "\n" + "https://apps.apple.com/ru/app/ready-for-sky/id927991375" + "\n\n" + "https://play.google.com/store/apps/details?id=com.readyforsky"
-//
-//                    let activityController: UIActivityViewController
-//
-//                    if let defaultPicture = UIImage(named: self.recipe.recipeImages)
-//                    { activityController = UIActivityViewController(activityItems: [ defaultText, defaultPicture], applicationActivities: nil) }
-//                    else
-//                    { activityController = UIActivityViewController(activityItems: [ defaultText], applicationActivities: nil) }
-//
-//                    self.present(activityController, animated: true, completion: nil)
-//
-//        })
-//        optionMenu.addAction(shareRecipeAction)
-//
-//         //Add cancel action
-//         let cancelAction = UIAlertAction(title: "Отмена", style: .cancel, handler: nil)
-//
-//         optionMenu.addAction(cancelAction)
-//
-//         // Display the menu
-//         present(optionMenu, animated: true, completion: nil)
-//
-//    }
+
+        //Add Share recipe action. Делимся рецептом с друзьями.
+        let shareRecipeAction = UIAlertAction(title: "Поделиться", style: .default, handler: {
+            (action:UIAlertAction!) -> Void in
+
+            let defaultText = "Рекомендую попробовать:" + "\n" + self.recipe.recipeNames + "\n" + "Кухня: " + self.recipe.recipeType + "\n" + "Автор рецепта: " + self.recipe.recipeAuthorLocations + "\n" + "Способ приготовления: " + "\n" + self.recipe.recipeDescription + "\n\n" + "Состав блюда:" + "\n" + self.recipe.recipeIngredients + "\n\n" + "Рецепт доступен в мобильном приложении:" + "\n" + "https://apps.apple.com/ru/app/ready-for-sky/id927991375" + "\n\n" + "https://play.google.com/store/apps/details?id=com.readyforsky"
+
+                    let activityController: UIActivityViewController
+
+                    if let defaultPicture = UIImage(named: self.recipe.recipeImages)
+                    { activityController = UIActivityViewController(activityItems: [ defaultText, defaultPicture], applicationActivities: nil) }
+                    else
+                    { activityController = UIActivityViewController(activityItems: [ defaultText], applicationActivities: nil) }
+
+                    self.present(activityController, animated: true, completion: nil)
+
+        })
+        optionMenu.addAction(shareRecipeAction)
+
+         //Add cancel action
+         let cancelAction = UIAlertAction(title: "Отмена", style: .cancel, handler: nil)
+
+         optionMenu.addAction(cancelAction)
+
+         // Display the menu
+         present(optionMenu, animated: true, completion: nil)
+
+    }
 
 }
